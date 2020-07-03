4d9e663cf1c2db9c9f72d336c2267325c296d745
Edson Silva-Filho <edsonsilva@edsons-mbp-2.home>
2020-07-03 13:19:11 -0500

    testing messages form

---
+++ src/components/message-form/message-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MessageForm() {
+    return (
+        <div>
+            Message Form
+        </div>
+    )
+}
+
+export default MessageForm
--- src/components/stand-up/stand-up.tsx
+++ src/components/stand-up/stand-up.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import Message from '../message/message';
-import { Layout } from "antd";
+import { Layout, Input, Space } from "antd";
 import Header from '../header/header';
+
+const { Search } = Input;
 
 function StandUp() {
 
 
     return (
-        <Layout >
-            <Header></Header>
-        </Layout>
+        <>
+            <Layout >
+                <Header></Header>
+            </Layout>
+            <Layout>
+                <Layout.Content>
+                    <Search
+                        placeholder="input search text"
+                        enterButton="Search"
+                        size="large"
+                        onSearch={value => console.log(value)}
+                    />
+                </Layout.Content>
+            </Layout>
+        </>
+
     );
 }
 
